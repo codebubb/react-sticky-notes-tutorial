c1642724a01a762ce9e5d54d68421253491c75bb
James Bubb <jpbubb82@gmail.com>
2021-04-30 13:04:32 +0100

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-require('file-loader?name=[name].[ext]!./index.html');
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-import './App.scss';
-
-const appElement = document.getElementById('app');
-
-ReactDOM.render(<App />, appElement);
